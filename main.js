f042e98d68c2aca0faca372269270dafa54d520a
Taiki Endo <te316e89@gmail.com>
2021-02-13 00:56:33 +0900

    Pass --noprofile and --norc options to bash

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const { execFileSync } = require('child_process');
 
 function main() {
   try {
-    execFileSync('bash', [ `${__dirname}/main.sh` ], { stdio : 'inherit' });
+    execFileSync('bash', [ '--noprofile', '--norc', `${__dirname}/main.sh` ],
+                 { stdio : 'inherit' });
   } catch (e) {
     console.log(`::error::${e.message}`);
     process.exit(1);
