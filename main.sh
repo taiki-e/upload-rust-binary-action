c50f60643cca7c1d16802795d987ec62178a6b54
Taiki Endo <te316e89@gmail.com>
2021-02-28 03:32:32 +0900

    Reformat scripts

--- main.sh
+++ main.sh
@@ -3,69 +3,65 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-function error {
-  if [[ -n "${GITHUB_ACTIONS:-}" ]]; then
+error() {
     echo "::error::$*"
-  else
-    echo "error: $*" >&2
-  fi
 }
 
 if [[ "${GITHUB_REF:?}" != "refs/tags/"* ]]; then
-  error "GITHUB_REF should start with 'refs/tags/'"
-  exit 1
+    error "GITHUB_REF should start with 'refs/tags/'"
+    exit 1
 fi
 tag="${GITHUB_REF#refs/tags/}"
 
 archive="${INPUT_ARCHIVE:?}"
 package=${INPUT_BIN:?}
 if [[ ! "${INPUT_TAR}" =~ ^(all|unix|windows|none)$ ]]; then
-  error "invalid input 'tar': ${INPUT_TAR}"
-  exit 1
+    error "invalid input 'tar': ${INPUT_TAR}"
+    exit 1
 elif [[ ! "${INPUT_ZIP}" =~ ^(all|unix|windows|none)$ ]]; then
-  error "invalid input 'zip': ${INPUT_ZIP}"
-  exit 1
+    error "invalid input 'zip': ${INPUT_ZIP}"
+    exit 1
 elif [[ "${INPUT_TAR}" == "none" ]] && [[ "${INPUT_ZIP}" == "none" ]]; then
-  error "at least one of INPUT_TAR or INPUT_ZIP must be a value other than 'none'"
-  exit 1
+    error "at least one of INPUT_TAR or INPUT_ZIP must be a value other than 'none'"
+    exit 1
 fi
 
 host=$(rustc -Vv | grep host | sed 's/host: //')
 target="${INPUT_TARGET:-"${host}"}"
 cargo="cargo"
 if [[ "${host}" != "${target}" ]]; then
-  rustup target add "${target}"
-  case "${target}" in
-    # https://github.com/rust-embedded/cross#supported-targets
-    *windows-msvc | *windows-gnu | *darwin | *fuchsia | *redox) ;;
-    *)
-      cargo="cross"
-      cargo install cross
-      ;;
-  esac
+    rustup target add "${target}"
+    case "${target}" in
+        # https://github.com/rust-embedded/cross#supported-targets
+        *windows-msvc | *windows-gnu | *darwin | *fuchsia | *redox) ;;
+        *)
+            cargo="cross"
+            cargo install cross
+            ;;
+    esac
 fi
 
 case "${OSTYPE}" in
-  linux*)
-    platform="unix"
-    strip="strip"
-    ;;
-  darwin*)
-    platform="unix"
-    strip="strip"
-    # Work around https://github.com/actions/cache/issues/403 by using GNU tar
-    # instead of BSD tar.
-    brew install gnu-tar
-    export PATH=${PATH}:/usr/local/opt/gnu-tar/libexec/gnubin
-    ;;
-  cygwin* | msys*)
-    platform="windows"
-    exe=".exe"
-    ;;
-  *)
-    error "unrecognized OSTYPE: ${OSTYPE}"
-    exit 1
-    ;;
+    linux*)
+        platform="unix"
+        strip="strip"
+        ;;
+    darwin*)
+        platform="unix"
+        strip="strip"
+        # Work around https://github.com/actions/cache/issues/403 by using GNU tar
+        # instead of BSD tar.
+        brew install gnu-tar
+        export PATH=${PATH}:/usr/local/opt/gnu-tar/libexec/gnubin
+        ;;
+    cygwin* | msys*)
+        platform="windows"
+        exe=".exe"
+        ;;
+    *)
+        error "unrecognized OSTYPE: ${OSTYPE}"
+        exit 1
+        ;;
 esac
 bin="${package}${exe:-}"
 
@@ -77,26 +73,26 @@ archive="${archive/\$target/${target}}"
 archive="${archive/\$tag/${tag}}"
 assets=()
 if [[ -n "${strip:-}" ]]; then
-  strip "${bin}"
+    strip "${bin}"
 fi
 if [[ "${INPUT_TAR/all/${platform}}" == "${platform}" ]]; then
-  assets+=("${archive}.tar.gz")
-  tar czf ../../../"${assets[0]}" "${bin}"
+    assets+=("${archive}.tar.gz")
+    tar czf ../../../"${assets[0]}" "${bin}"
 fi
 if [[ "${INPUT_ZIP/all/${platform}}" == "${platform}" ]]; then
-  assets+=("${archive}.zip")
-  if [[ "${platform}" == "unix" ]]; then
-    zip ../../../"${archive}.zip" "${bin}"
-  else
-    7z a ../../../"${archive}.zip" "${bin}"
-  fi
+    assets+=("${archive}.zip")
+    if [[ "${platform}" == "unix" ]]; then
+        zip ../../../"${archive}.zip" "${bin}"
+    else
+        7z a ../../../"${archive}.zip" "${bin}"
+    fi
 fi
 cd ../../..
 
 if [[ -z "${GITHUB_TOKEN:-}" ]]; then
-  error "GITHUB_TOKEN not set, skipping deploy"
-  exit 1
+    error "GITHUB_TOKEN not set, skipping deploy"
+    exit 1
 else
-  # https://cli.github.com/manual/gh_release_upload
-  gh release upload "${tag}" "${assets[@]}" --clobber
+    # https://cli.github.com/manual/gh_release_upload
+    gh release upload "${tag}" "${assets[@]}" --clobber
 fi
--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -2,20 +2,23 @@
 
 # Format all scripts.
 #
-# Note: This script requires shfmt and clang-format.
+# Note: This script requires shfmt and prettier.
 
 set -euo pipefail
 IFS=$'\n\t'
 
 cd "$(cd "$(dirname "${0}")" && pwd)"/..
 
-if [[ -n "${CI:-}" ]]; then
-  shfmt -d ./*/*.sh ./*.sh
+if [[ -z "${CI:-}" ]]; then
+    (
+        set -x
+        shfmt -l -w ./*/*.sh ./*.sh
+        prettier -w ./*.js
+    )
 else
-  shfmt -l -w ./*/*.sh ./*.sh
-fi
-
-clang-format -i ./*.js
-if [[ -n "${CI:-}" ]]; then
-  git diff --exit-code
+    (
+        set -x
+        shfmt -d ./*/*.sh ./*.sh
+        prettier -c ./*.js
+    )
 fi
