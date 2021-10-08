c456fc5a79d3975ed814351a0a9cee0312b35bad
Taiki Endo <te316e89@gmail.com>
2021-10-09 04:04:46 +0900

    Update tools/publish.sh

--- tools/publish.sh
+++ tools/publish.sh
@@ -11,14 +11,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+cd "$(cd "$(dirname "$0")" && pwd)"/..
+
 error() {
     echo "error: $*" >&2
 }
-
-cd "$(cd "$(dirname "$0")" && pwd)"/..
-
-git diff --exit-code
-git diff --exit-code --staged
 
 # Parse arguments.
 version="${1:?}"
@@ -36,19 +33,21 @@ if [[ -n "${2:-}" ]]; then
     exit 1
 fi
 
+if [[ -z "${dry_run:-}" ]]; then
+    git diff --exit-code
+    git diff --exit-code --staged
+fi
+
 # Make sure that a valid release note for this version exists.
 # https://github.com/taiki-e/parse-changelog
-echo "========== changes =========="
+echo "============== CHANGELOG =============="
 parse-changelog CHANGELOG.md "${version}"
-echo "============================="
+echo
+echo "======================================="
 
 # Make sure the same release has not been created in the past.
 if gh release view "${tag}" &>/dev/null; then
     error "tag '${tag}' has already been created and pushed"
-    exit 1
-fi
-if git --no-pager tag | grep "${tag}" &>/dev/null; then
-    error "tag '${tag}' has already been created"
     exit 1
 fi
 
