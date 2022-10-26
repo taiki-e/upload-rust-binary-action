b6eda09921fc280141fb7f0a76cd94409fbe169f
George Hahn <george.hahn@datadoghq.com>
2022-10-26 16:09:54 -0600

    Use `cargo metadata` to get target directory

--- main.sh
+++ main.sh
@@ -181,11 +181,7 @@ if [[ -n "${strip:-}" ]]; then
 fi
 
 build_options=("--release")
-target_dir="target/release"
-if [[ -n "${INPUT_TARGET:-}" ]]; then
-    target_dir="target/${target}/release"
-    build_options+=("--target" "${target}")
-fi
+target_dir=""
 bins=()
 for bin_name in "${bin_names[@]}"; do
     bins+=("${bin_name}${exe:-}")
@@ -200,8 +196,15 @@ fi
 manifest_path="${INPUT_MANIFEST_PATH:-}"
 if [[ -n "${manifest_path}" ]]; then
     build_options+=("--manifest-path" "${manifest_path}")
-    build_options+=("--target-dir" "${RUNNER_TEMP}/target")
-    target_dir="${RUNNER_TEMP}/${target_dir}"
+    target_dir=$(cargo metadata --format-version=1 --no-deps --manifest-path "${manifest_path}" | jq -r '."target_directory"')
+else
+    target_dir=$(cargo metadata --format-version=1 --no-deps | jq -r '."target_directory"')
+fi
+if [[ -n "${INPUT_TARGET:-}" ]]; then
+    build_options+=("--target" "${target}")
+    target_dir="${target_dir}/${target}/release"
+else
+    target_dir="${target_dir}/release"
 fi
 
 case "${build_tool}" in
