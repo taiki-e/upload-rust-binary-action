7220b29080d9e29e8fdb0e8530cbaa9db1c77237
linrongbin16 <linrongbin16@outlook.com>
2024-04-09 19:11:32 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -347,7 +347,7 @@ if [[ "${INPUT_TAR/all/${platform}}" == "${platform}" ]] || [[ "${INPUT_ZIP/all/
     mkdir "${tmpdir:?}/${archive}"
     filenames=("${bins[@]}")
     for bin_exe in "${bins[@]}"; do
-        if [[ -z "${bin_leading_dir}" ]]; then
+        if [[ -n "${bin_leading_dir}" ]]; then
             x mkdir -p "${tmpdir}/${bin_leading_dir}"/
             x cp "${target_dir}/${bin_exe}" "${tmpdir}/${bin_leading_dir}/${archive}"/
         else
